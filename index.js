a63c076817bf6db049f97d11852538089991fc7e
kacto1973 <a222212359@unison.mx>
2026-03-16 13:28:11 -0700

    feat: v1.0

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+const SECRET_KEY = "clave_secreta_y_segura";
+
+const users = [{ id: 1, username: "admin", password: "123" }];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (u) => u.username === username && u.password === password,
+  );
+
+  if (!user) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
+  }
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true, // El JavaScript del navegador no puede leer esta cookie. pal xss
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60, // 1 hora de validez
+  });
+
+  res.json({ message: "Login exitoso, cookie establecida" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(
+    `Servidor de autenticación corriendo en http://localhost:${PORT}`,
+  );
+});
